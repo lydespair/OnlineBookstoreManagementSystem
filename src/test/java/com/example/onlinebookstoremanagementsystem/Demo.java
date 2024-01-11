5812a40f269d3fb339baf61cfe4e6a64c380c95d
Ptfty <2310957061@qq.com>
2024-01-11 14:41:53 +0800

    all

--- src/test/java/com/example/onlinebookstoremanagementsystem/Demo.java
+++ src/test/java/com/example/onlinebookstoremanagementsystem/Demo.java
@@ -2,7 +2,6 @@ package com.example.onlinebookstoremanagementsystem;
 
 import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSS;
-import com.aliyun.oss.common.auth.*;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.OSSException;
 import com.aliyun.oss.model.PutObjectRequest;
